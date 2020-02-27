fa0fb984849b39cc7a62faaaa194165d9514b5cd
jnoguerapavon <julio.noguera.pavon@gmail.com>
2020-02-26 20:32:54 -0600

    first commit

---
+++ src/main/java/org/Test/Test/App.java
@@ -0,0 +1,13 @@
+package org.Test.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello 	Perro!" );
+    }
+}
